86980a9f8db8e15e49dd81b4a125ee50172a7058
David Martinez <davidmgilo@gmail.com>
2017-03-08 19:55:34 +0100

    events

--- client/index.js
+++ client/index.js
@@ -1,10 +1,11 @@
 var socket = require('socket.io-client')('http://localhost:3000');
 socket.on('connect', function(){
-
+    console.log('Client connected ok to localhost port 3000')
 });
-socket.on('event', function(data){
-
+socket.on('pepita', function(data){
+    console.log('Event pepita received')
+    console.log(data)
 });
 socket.on('disconnect', function(){
-
+    console.log('Client disconnected ok from localhost port 3000')
 });
--- server/standalone.js
+++ server/standalone.js
@@ -1,5 +1,8 @@
 var io = require('socket.io')();
 io.on('connection', function(client){
     console.log('New connection received')
+    console.log(client)
+    client.emit('pepita','prova')
+    client.disconnect()
 });
 io.listen(3000);
