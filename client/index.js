4c5911b67e3e035a7a237f60633f9bb54a51c21e
David Martinez <davidmgilo@gmail.com>
2017-03-08 19:42:07 +0100

    client

---
+++ client/index.js
@@ -0,0 +1,10 @@
+var socket = require('socket.io-client')('http://localhost:3000');
+socket.on('connect', function(){
+
+});
+socket.on('event', function(data){
+
+});
+socket.on('disconnect', function(){
+
+});
